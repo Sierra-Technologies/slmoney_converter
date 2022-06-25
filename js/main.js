ebb381348060e93073e81160ae112fa53ab3afd7
Emmanuel Kamanda <56160669+Emmanuel1255@users.noreply.github.com>
2022-06-25 11:35:17 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function moneyConverter(valNum) {
+    document.getElementById("newmoney").innerHTML=valNum/1000;
+  }
+  function moneyConverter2(valNum) {
+    document.getElementById("oldmoney").innerHTML=valNum*1000;
+  }
+  
