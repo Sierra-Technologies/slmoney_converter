c1fb3a85c95a63f7dc3ba1b72e484d7b1ad6cbf0
Emmanuel Kamanda <56160669+Emmanuel1255@users.noreply.github.com>
2022-06-27 01:21:34 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 function moneyConverter(valNum) {
-    document.getElementById("newmoney").innerHTML=valNum/1000;
+    num = valNum/1000
+    loc = num.toLocaleString("en-US");
+    console.log(loc)
+    document.getElementById("newmoney").innerHTML=loc;
   }
-  function moneyConverter2(valNum) {
-    document.getElementById("oldmoney").innerHTML=valNum*1000;
+function moneyConverter2(valNum) {
+    num = valNum*1000
+    loc = num.toLocaleString("en-US");
+    document.getElementById("oldmoney").innerHTML=loc;
+}
+
+function commaMon(valN){
+  x=valN.toLocaleString('en-US')
+  console.log(x)
+}
+
+  let btnConvet = document.querySelector('#con');
+  let input = document.querySelector('#oldd');
+  let output = document.querySelector('h2');
+  btnConvet.addEventListener('click', () => {
+      output.innerText = numberToWords.toWords(input.value),"Leones";
   }
+  )
+
+
+
+
   
